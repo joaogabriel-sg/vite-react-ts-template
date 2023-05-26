793d3d8660666d4864052c67e950165aecf6b83d
João Gabriel <main.joaogabrielsg@gmail.com>
2023-05-26 13:20:45 -0300

    feat: create query client instance

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,16 @@
 import { RouterProvider } from 'react-router-dom';
 
+import { QueryClientProvider } from '@tanstack/react-query';
+
+import { queryClient } from './lib/query';
 import { router } from './routes';
 
 function App() {
-  return <RouterProvider router={router} />;
+  return (
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+    </QueryClientProvider>
+  );
 }
 
 export default App;
